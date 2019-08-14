5ad87020a8135a47c454ba29b9535418b6ea5c88
pablodezutter <dezutterpablo@gmail.com>
2019-08-14 22:29:16 +0200

    html, css

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # RockPaperScissor
+
+Big thanks to Dev Ed! 
+https://www.youtube.com/watch?v=qWPtKtYEsN4&list=PLDyQo7g0_nsX8_gZAB8KD1lL4j4halQBJ&index=10
---
+++ rock-paper-scissor-master/app.js
@@ -0,0 +1,14 @@
+//scoping everything//
+const game = () =>{
+  let pScore= 0;
+  let cScore= 0;
+
+  const startGame = () =>{
+    const playButton = document.querySelector(".intro button");
+  }
+
+
+
+
+
+}
