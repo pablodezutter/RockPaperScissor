0efc4e684549ec95853d0108b19ab6064f9d29a3
pablodezutter <dezutterpablo@gmail.com>
2019-10-01 21:44:27 +0200

    create functions, add transition

--- rock-paper-scissor-master/app.js
+++ rock-paper-scissor-master/app.js
@@ -5,10 +5,17 @@ const game = () =>{
 
   const startGame = () =>{
     const playButton = document.querySelector(".intro button");
-  }
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector('.match');
 
+    playButton.addEventListener('click', () => {
+      introScreen.classList.add('fadeOut');
+      match.classList.add('fadeIn');
+  });
+};
+startGame();
+console.log('yeah');
+};
 
-
-
-
-}
+game();
+console.log('ready');
